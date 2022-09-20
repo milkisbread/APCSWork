3172ca098ade741f58a1752a06ba1f946df6e915
milkisbread <chloekim24@marlborough.org>
2022-09-20 15:05:44 -0700

    Unit 1

--- Unit1/Fibonacci/Fibonacci.java
+++ Unit1/Fibonacci/Fibonacci.java
@@ -2,10 +2,13 @@ public class Fibonacci
 {
   public static void main(String args[])
   {
-    int a, b, c;
-    for(i=0; i<30; i++)
+    int a = 0, b = 1, c;
+    for(int i=0; i<30; i++)
     {
-      System.out.println(c = a+b);
+      c = a+b;
+      System.out.println(c);
+      a = b;
+      b = c;
     }
   }
 }
